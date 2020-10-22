d4a536d0dae9b7ddd398f9c193984b42ad83c8cc
Jake Levy <jake.r.levy@gmail.com>
2020-10-21 21:36:50 -0400

    Working DB Connection

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-var mongoClient = require("mongodb").MongoCleint;
-var tracker = require("tracker");
-
-var url = "mongodb://localhost:27017/activitiesDB"
+var mongoClient = require("mongodb").MongoClient;
 
 
+var url = "mongodb://127.0.0.1:27017/"
+
+    const client = new mongoClient(url);
+
+async function run(){
+
+    try{
+	await client.connect({useUnifiedTopology: true});
+	
+
+       const actDB = client.db("practiceDB");
+       const activities = actDB.collection("activities");
+
+       const query = { distance: { $lt: 5 }};
+
+       const cursor = activities.find(query, {
+	       projection: { _id:0 , activity: 1, distance: 1}});
+
+       if (( await cursor.count())== 0){
+	   console.log("No docs found!");
+       }
+
+       await cursor.forEach(console.dir);
+    } finally{
+	await client.close();
+    }
+
+}
+
+run().catch(console.dir);
