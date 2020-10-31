e4366dcc8131066a518d650dd07708a6149d216e
Jake Levy <jake.r.levy@gmail.com>
2020-10-30 23:10:02 -0400

    DB connected through dbManager file

---
+++ backups/test.js
@@ -0,0 +1,16 @@
+
+const dbManager = require('./dbManager.js');
+
+//I THINK THIS WORKED
+//call the get method 
+async function main(){
+    try{
+
+	let ourDB = await dbManager.get(); //after this line we are connected.  Notice that the async connect function can be awaited
+	//if we put the call in another async function
+    }
+    
+}
+
+main();
+//dbManager.get().;
